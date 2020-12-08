75007233bc989e0d43a55c53095817d1f90aa9a8
shane <shanecolbyzero8@gmail.com>
2020-12-07 20:43:32 -0600

    add v-if box that toggles on/off with click

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ var app = new Vue({
   data: function () {
     return {
       message: "Hello from JavaScript!",
+      showInfo: true,
     };
   },
   methods: {
     changeMessage: function () {
       console.log("changing message...");
       this.message = "POOF...new message";
+    },
+    toggleInfo: function () {
+      console.log('toggling info...');
+      this.showInfo = !this.showInfo;
+      // if (this.showInfo === true) {
+      //   this.showInfo = false;
+      // } else {
+      //   this.showInfo = true;
+      // }
     }
   }
 });
